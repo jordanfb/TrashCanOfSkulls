e8655108f19bb816693fa7aed5c0f6c08a9a9dba
Jon Castro <castrj5@rpi.edu>
2018-10-30 23:56:38 -0400

    Created movement and basic scene setup
    added particles / terrain / lighting / skybox

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+
+    public float speed;
+    private Rigidbody rgd;
+    public float rotateSpeedX;
+    public float rotateSpeedY;
+    private Vector3 mousePos;
+    private float rotateX;
+    private float rotateY;
+
+    // Use this for initialization
+    void Start () {
+        rgd = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+        }
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
+        {
+            transform.Translate(0, 0, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+        }
+        rotateX += rotateSpeedX * Input.GetAxis("Mouse X");
+        rotateY -= rotateSpeedY * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(rotateY, rotateX, 0);
+    }
+}
---
+++ Assets/Scripts/pickUp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pickUp : MonoBehaviour {
+
+    public Text txt;
+    public Text noteCount;
+    public Image img;
+
+    // Use this for initialization
+    void Start () {
+        img.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (img.enabled == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                //increment count here if you want
+                img.enabled = false;
+                txt.text = "";
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Notebook")
+        {
+            txt.text = "Press E to pick up";
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                img.sprite = other.GetComponent<SpriteRenderer>().sprite;
+                img.enabled = true;
+                //Also can increment count here
+                Destroy(other.gameObject);
+                txt.text = "Press LMB to close";
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Notebook")
+        {
+            txt.text = "";
+        }
+    }
+}

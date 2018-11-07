b8d51e08dad143f6a6d1c731701107fa980433de
Jordan Faas-Bush <jmanfb@gmail.com>
2018-11-07 03:41:50 -0500

    Created a trigger script that invokes a unityEvent when a trigger is entered by the player

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerScript : MonoBehaviour {
+
+    [SerializeField]
+    private bool requirePlayerHasCure = false;
+    [SerializeField]
+    private pickUp pickUpScript;
+    [SerializeField]
+    private bool oneTimeTrigger = true;
+
+    public UnityEvent triggerEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            Debug.Log("Player hit trigger");
+            if (!requirePlayerHasCure || false)
+            {
+                // then run the event
+                Debug.Log("Trigger Triggered");
+                triggerEvent.Invoke();
+                if (oneTimeTrigger)
+                {
+                    this.enabled = false; // disable yourself. we could also disable the gameobject but this is slightly safer in case we stick this where we shouldn't.
+                }
+            }
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

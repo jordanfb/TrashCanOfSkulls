8ac59da3c06338cbfd01e6775d1f89bd05e41762
Xuan Liu <seamasliu@gmail.com>
2018-11-07 00:32:32 -0500

    Got refilling vials working.

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -6,9 +6,10 @@ public class Throw : MonoBehaviour {
     public GameObject vial;
     public GameObject playerCamera;
 
-    public int amount;
     public Vector3 vialPosition;
     public float vialThrowingForce;
+
+    public int vialAmount = 0;
 
     // Use this for initialization
     void Start() {
@@ -18,14 +19,14 @@ public class Throw : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            if (amount > 0) {
+            if (vialAmount > 0) {
                 GameObject instance = Instantiate(vial, playerCamera.transform.position 
                                                       + playerCamera.transform.forward * vialPosition.z
                                                       + playerCamera.transform.right * vialPosition.y
                                                       + playerCamera.transform.up * vialPosition.x, playerCamera.transform.rotation);
                 instance.GetComponent<Rigidbody>().useGravity = true;
                 instance.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * vialPosition.z * vialThrowingForce);
-                amount -= 1;
+                vialAmount -= 1;
             }
         }
     }

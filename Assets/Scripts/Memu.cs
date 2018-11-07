ce8710d421481fbda6f97027195458fbbf7e875e
Xuan Liu <seamasliu@gmail.com>
2018-11-07 01:53:26 -0500

    Got menu working.

---
+++ Assets/Scripts/Memu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Memu : MonoBehaviour {
+
+    public GameObject button;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown() {
+        if (button.name == "Play") {
+            SceneManager.LoadScene("Master");
+        }
+        if (button.name == "Quit") {
+            Application.Quit();
+        }
+    }
+
+}

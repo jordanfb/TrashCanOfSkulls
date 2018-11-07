35f5f45513d7fe2b043ef6c9159f57c475b7e4bc
Jordan Faas-Bush <jmanfb@gmail.com>
2018-11-07 04:42:06 -0500

    Vials should now stun enemies

---
+++ Assets/Scripts/VialScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VialScript : MonoBehaviour {
+
+    [SerializeField]
+    private float explosionRadius;
+    [SerializeField]
+    private ParticleSystem particleSys;
+    [SerializeField]
+    private LayerMask sphereCastLayerMask;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("VIAL HIT SOMETHING OH GOD KILL EVERYTHING");
+        // then circle cast to see if we can hit the enemy nearby
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, explosionRadius, transform.forward, out hit, 0, sphereCastLayerMask, QueryTriggerInteraction.Collide))
+        {
+            RatController rat = hit.transform.GetComponent<RatController>();
+            Debug.Log("VIAL HIT ENEMY");
+            if (rat)
+            {
+                // then stun the rat
+                rat.StunRat();
+            }
+        }
+    }
+}

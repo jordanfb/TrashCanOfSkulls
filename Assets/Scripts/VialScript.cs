e86c34ce88769f9e89834b902dc9e4a5b7436812
Jordan Faas-Bush <jmanfb@gmail.com>
2018-11-07 04:55:45 -0500

    Vials now have a particle system

--- Assets/Scripts/VialScript.cs
+++ Assets/Scripts/VialScript.cs
@@ -11,19 +11,31 @@ public class VialScript : MonoBehaviour {
     [SerializeField]
     private LayerMask sphereCastLayerMask;
 
+    private bool readyToExplode = true;
+
+    public void Start()
+    {
+        particleSys.Stop();
+    }
+
     public void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("VIAL HIT SOMETHING OH GOD KILL EVERYTHING");
-        // then circle cast to see if we can hit the enemy nearby
-        RaycastHit hit;
-        if (Physics.SphereCast(transform.position, explosionRadius, transform.forward, out hit, 0, sphereCastLayerMask, QueryTriggerInteraction.Collide))
+        if (readyToExplode)
         {
-            RatController rat = hit.transform.GetComponent<RatController>();
-            Debug.Log("VIAL HIT ENEMY");
-            if (rat)
+            readyToExplode = false;
+            Debug.Log("VIAL HIT SOMETHING OH GOD KILL EVERYTHING");
+            particleSys.Play();
+            // then circle cast to see if we can hit the enemy nearby
+            RaycastHit hit;
+            if (Physics.SphereCast(transform.position, explosionRadius, transform.forward, out hit, 0, sphereCastLayerMask, QueryTriggerInteraction.Collide))
             {
-                // then stun the rat
-                rat.StunRat();
+                RatController rat = hit.transform.GetComponent<RatController>();
+                Debug.Log("VIAL HIT ENEMY");
+                if (rat)
+                {
+                    // then stun the rat
+                    rat.StunRat();
+                }
             }
         }
     }

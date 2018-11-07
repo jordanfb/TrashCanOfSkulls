61e4a6fcb9895871b09f291668728bdb8bce19fb
Jordan Faas-Bush <jmanfb@gmail.com>
2018-11-06 22:46:34 -0500

    Added a level manager to lock mouse and eventually exit to menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // on start of the level
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            // quit to the main menu
+            //ExitToMenu();
+        }
+	}
+
+
+    void ExitToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
